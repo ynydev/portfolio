e5cd52e93d86082bb4c45ca7a254bfe3a63aa50a
YNYDev <ynto.dev@gmail.com>
2025-11-14 05:24:40 +0000

    変えた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Button from "@/components/ui/Button";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full px-14 space-y-[30px]">
         <div className="flex flex-col items-start justify-start">
-          <h1 className="text-4xl">Web design that combines usability and beauty</h1>
-          <p className="text-2xl text-[#cbcbcb]">I am YNYDev</p>
+          <h1 className="lg:text-5xl md:text-4xl text-3xl">Web design that combines usability and beauty</h1>
+          <p className="md:text-2xl text-xl text-[#cbcbcb]">I am YNYDev</p>
         </div>
         <div className="flex items-center gap-4">
           <Button variant="primary" href="/about">About me</Button>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
       <div className="flex items-center justify-end">
         <button
           onClick={() => setIsMenuOpen(prevState => !prevState)}
-          className="relative w-6 h-4 flex flex-col justify-between items-center group z-120 outline-none"
+          className="relative w-6 h-4 flex flex-col justify-between items-center group z-120 outline-none cursor-pointer"
         >
           <span
             className={`
@@ -43,7 +43,8 @@ export default function Navbar() {
         <nav className={`
           ${isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"}
           fixed inset-0 z-100 flex items-center justify-center
-          bg-black w-full h-full duration-500 grid grid-rows-1 md:grid-cols-2
+          bg-[rgba(0,0,0,0.75)] w-full h-full duration-500 grid grid-rows-1 md:grid-cols-2
+          backdrop-blur-sm
         `}>
           <div className="flex justify-center">
             <div className="group grid grid-rows-3 gap-8">
@@ -51,7 +52,7 @@ export default function Navbar() {
                 <Link
                   key={links.href}
                   href={links.href}
-                  className={`${spaceMono.className} text-6xl group-hover:text-[#a7a7a7] hover:text-white duration-300`}
+                  className={`${spaceMono.className} text-6xl group-hover:text-[#a1a1a1] hover:text-white duration-300`}
                 >
                   {links.text}
                 </Link>
