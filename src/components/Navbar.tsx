7422ba2c777e1680b2100a276eefa1a65cc9b0c7
YNYDev <ynto.dev@gmail.com>
2025-11-14 01:47:02 +0000

    レスポンシブ対応にした

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,64 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+import { Space_Mono } from "next/font/google";
 
 const mainNavLinks = [
-  { "title": "Home", "href": "/" },
-  { "title": "About", "href": "/about" },
-  { "title": "Works", "href": "/works" },
+  { "text": "Home", "href": "/" },
+  { "text": "About", "href": "/about" },
+  { "text": "Works", "href": "/works" },
 ];
 
+const spaceMono = Space_Mono({ weight: "400" });
+
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="px-14 fixed top-0 w-full flex grid w-full grid-cols-3 p-4">
+    <div className="flex items-center justify-between px-14 fixed top-0 w-full flex w-full py-6">
       <div className="flex items-center justify-start">
         <Link
           className="text-xl"
           href="/"
         >YNYDev</Link>
       </div>
-      <div className="flex items-center justify-center">
-        <div className="grid grid-cols-3 gap-2 items-center border border-[#444444] rounded-full p-1 overflow-hidden">
-          {mainNavLinks.map((links) => (
-            <div key={links.href} className="hover:shadow-[0_0_100px_50px_rgba(255,_255,_255,_0.2)] rounded-full duration-300">
-              <Link
-                className="flex justify-center px-5 py-1 hover:shadow-[inset_50px_50px_100px_50px_rgba(255,_255,_255,_0.2)] rounded-full duration-300"
-                href={links.href}
-              >{links.title}</Link>
-            </div>
-          ))}
-        </div>
-      </div>
       <div className="flex items-center justify-end">
-        <Link
-          href="https://discord.com/users/1436998440386297886"
+        <button
+          onClick={() => setIsMenuOpen(prevState => !prevState)}
+          className="relative w-6 h-4 flex flex-col justify-between items-center group z-120 outline-none"
         >
-          <span className="px-5 py-2 rounded-full bg-[#cfcfcf] hover:bg-white text-black duration-300">Contact</span>
-        </Link>
+          <span
+            className={`
+              block h-[1px] w-full bg-white transition-all duration-300
+              ${isMenuOpen ? "rotate-45 translate-y-[7.5px]" : ""}
+            `}
+          />
+          <span
+            className={`
+              block h-[1px] w-full bg-white transition-all duration-300
+              ${isMenuOpen ? "-rotate-45 -translate-y-[7.5px]" : ""}
+            `}
+          />
+        </button>
+        <nav className={`
+          ${isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"}
+          fixed inset-0 z-100 flex items-center justify-center
+          bg-black w-full h-full duration-500 grid grid-rows-1 md:grid-cols-2
+        `}>
+          <div className="flex justify-center">
+            <div className="group grid grid-rows-3 gap-8">
+              {mainNavLinks.map((links) => (
+                <Link
+                  key={links.href}
+                  href={links.href}
+                  className={`${spaceMono.className} text-6xl group-hover:text-[#a7a7a7] hover:text-white duration-300`}
+                >
+                  {links.text}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
   );
